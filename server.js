bbc93abff29fed2fe790b4fe0deedf7bcfcdb9c4
Belay Habitamu <belayhabitamu5@gmail.com>
2026-09-25 02:43:57 -0700

    Added server routes for about and more pages

--- server.js
+++ server.js
@@ -47,6 +47,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // --- EXPLICIT HTML ROUTES ---
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/admin.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html')));
+// NEW ROUTES ADDED HERE:
+app.get('/about.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about.html')));
+app.get('/more.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'more.html')));
 
 // --- DATABASE SETUP (PostgreSQL Production Upgrade) ---
 const sequelize = process.env.DATABASE_URL 
